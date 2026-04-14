c83dbd553f47323e4f7e1c78d983435f3083d69e
sh1n3sh1n3 <sh1n3sh1n3.03@gmail.com>
2026-04-14 06:50:48 -0700

    Merge cpanel

---
+++ config/config.staging.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'base_url' => 'https://interactsafety.com.au/',
+    'database' => [
+        'host' => 'localhost',
+        'port' => 3306,
+        'name' => 'interac8_coursehub',
+        'user' => 'interac8_coursehub',
+        'password' => 'AcZ&xNCzJii,8]g',
+        'charset' => 'utf8mb4'
+    ]
+];
